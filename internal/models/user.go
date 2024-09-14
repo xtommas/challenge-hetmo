41824d316800a780a531bf03a44abd9a5aad2ba7
Tomas <tomascanepa1397@gmail.com>
2024-09-14 12:13:43 -0300

    Created User model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,26 @@
 package models
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username" validate:"required,min=3,max=50"`
+	Password string `json:"-" validate:"required,min=6"`
+	IsAdmin  bool   `json:"is_admin"`
+}
+
+func (u *User) SetPassword(password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
