ea51605a86aed9cda43489502d417d1ce099417b
Tomas <tomascanepa1397@gmail.com>
2024-09-14 12:17:18 -0300

    Created User Repository

--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -1,1 +1,26 @@
 package repositories
+
+import (
+	"database/sql"
+
+	"github.com/xtommas/challenge-hetmo/internal/models"
+)
+
+type UserRepository struct {
+	DB *sql.DB
+}
+
+func (r *UserRepository) CreateUser(user *models.User) error {
+	query := `INSERT INTO users (username, password, is_admin) VALUES ($1, $2, $3) RETURNING id`
+	return r.DB.QueryRow(query, user.Username, user.Password, user.IsAdmin).Scan(&user.ID)
+}
+
+func (r *UserRepository) GetUserByUsername(username string) (*models.User, error) {
+	query := `SELECT id, username, password, is_admin FROM users WHERE username = $1`
+	user := &models.User{}
+	err := r.DB.QueryRow(query, username).Scan(&user.ID, &user.Username, &user.Password, &user.IsAdmin)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
