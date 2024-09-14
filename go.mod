23d014421255b84d15808602232046fd34f26b30
Tomas <tomascanepa1397@gmail.com>
2024-09-14 13:05:52 -0300

    Created Login and Register handlers

--- go.mod
+++ go.mod
@@ -11,9 +11,12 @@ require (
 require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/golang-migrate/migrate/v4 v4.18.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- internal/handlers/user_handler.go
+++ internal/handlers/user_handler.go
@@ -1,1 +1,75 @@
 package handlers
+
+import (
+	"database/sql"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+	"github.com/xtommas/challenge-hetmo/internal/models"
+	"github.com/xtommas/challenge-hetmo/internal/repositories"
+)
+
+func Register(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		user := new(models.User)
+		if err := c.Bind(user); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+		}
+
+		if err := c.Validate(user); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+		}
+
+		userRepo := repositories.UserRepository{DB: db}
+		err := userRepo.CreateUser(user)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to register user"})
+		}
+		return c.JSON(http.StatusCreated, user)
+	}
+}
+
+func Login(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var loginData struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		}
+		if err := c.Bind(&loginData); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid input"})
+		}
+
+		UserRepo := repositories.UserRepository{DB: db}
+		user, err := UserRepo.GetUserByUsername(loginData.Username)
+		if err != nil {
+			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Invalid credentials"})
+		}
+
+		if !user.CheckPassword(loginData.Password) {
+			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Invalid credentials"})
+		}
+
+		token := jwt.New(jwt.SigningMethodHS256)
+
+		// Set claims (the info that the JWT transmits)
+		claims := token.Claims.(jwt.MapClaims)
+		claims["id"] = user.ID
+		claims["username"] = user.Username
+		claims["is_admin"] = user.IsAdmin
+		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+		// Generate encoded token
+		t, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+		if err != nil {
+			return err
+		}
+
+		return c.JSON(http.StatusOK, map[string]string{
+			"token": t,
+		})
+
+	}
+}
